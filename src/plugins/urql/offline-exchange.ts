a80fbff0161004840848a1b1efa70d7fa0410a96
Trey <trey.rogers@rexelusa.com>
2024-08-29 13:14:29 -0400

    Enable line count example

--- src/plugins/urql/offline-exchange.ts
+++ src/plugins/urql/offline-exchange.ts
@@ -69,7 +69,7 @@ const offlineExchange = () => {
           })
         }
 
-        // returnCart.lineCount = returnCart.lines.length
+        returnCart.lineCount = returnCart.lines.length
 
         return returnCart
       },
